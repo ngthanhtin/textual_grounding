ad142dd4ff21bad4f07cd3dc1cb8d7ddeec9ed86
tin <ngthanhtinqn@gmail.com>
2024-11-06 06:53:03 -0600

    fix

---
+++ utils/eval_mmlu.py
@@ -0,0 +1,367 @@
+"""Response Parsing and Evaluation for various models"""
+from typing import Dict
+
+import sys, os, re, random, yaml
+sys.path.insert(0, '../')
+random.seed(42)
+import numpy as np
+import pandas as pd
+
+from arg_parser import get_common_args
+from utils import retrieve_gts
+
+# ----------- Process Multi-choice -------------
+def parse_multi_choice_response(response, all_choices, index2ans):
+    """
+    Parse the prediction from the generated response.
+    Return the predicted index e.g., A, B, C, D.
+    """
+    for char in [',', '.', '!', '?', ';', ':', "'"]:
+        response = response.strip(char)
+    response = " " + response + " " # add space to avoid partial match
+
+    index_ans = True
+    ans_with_brack = False
+    candidates = []
+    for choice in all_choices:  # e.g., (A) (B) (C) (D)
+        if f'({choice})' in response:
+            candidates.append(choice)
+            ans_with_brack = True
+        if '{' + f'{choice}' + '}' in response:
+            candidates.append(choice)
+            ans_with_brack = True
+
+    if len(candidates) == 0:
+        for choice in all_choices: # e.g., A B C D
+            if f' {choice} ' in response:
+                candidates.append(choice)
+
+    # if all above doesn't get candidates, check if the content is larger than 5 tokens and try to parse the example
+    if len(candidates) == 0 and len(response.split()) > 5:
+        for index, ans in index2ans.items():
+            if ans.lower() in response.lower():
+                candidates.append(index)
+                index_ans = False # it's content ans.
+
+    if len(candidates) == 0:  # still not get answer, randomly choose one.
+        pred_index = random.choice(all_choices)
+    elif len(candidates) > 1:
+        start_indexes = []
+        if index_ans:
+            if ans_with_brack: 
+                for can in candidates:
+                    index = response.rfind(f'({can})')
+                    start_indexes.append(index) # -1 will be ignored anyway
+                # start_indexes = [generated_response.index(f'({can})') for can in candidates]
+            else:
+                for can in candidates:
+                    index = response.rfind(f" {can} ")
+                    start_indexes.append(index)
+        else:
+            for can in candidates:
+                index = response.lower().rfind(index2ans[can].lower())
+                start_indexes.append(index)
+        # get the last one
+        pred_index = candidates[np.argmax(start_indexes)]
+    else: # if only one candidate, use it.
+        pred_index = candidates[0]
+
+    return pred_index
+
+# ----------- Process Open -------------
+def check_is_number(string):
+    """
+    Check if the given string a number.
+    """
+    try:
+        float(string.replace(',', ''))
+        return True
+    except ValueError:
+        # check if there's comma inside
+        return False
+
+def normalize_str(string):
+    """
+    Normalize the str to lower case and make them float numbers if possible.
+    """
+    # check if characters in the string
+
+    # if number, numerize it.
+    string = string.strip()
+
+    is_number = check_is_number(string)
+
+    if is_number:
+        string = string.replace(',', '')
+        string = float(string)
+        # leave 2 decimal
+        string = round(string, 2)
+        return [string]
+    else: # it's likely to be a string
+        # lower it 
+        string = string.lower()
+        if len(string) == 1:
+            return [" " + string, string + " "] # avoid trivial matches
+        return [string]
+
+def extract_numbers(string):
+    """
+    Exact all forms of numbers from a string with regex.
+    """
+    # Pattern for numbers with commas
+    pattern_commas = r'-?\b\d{1,3}(?:,\d{3})+\b'
+    # Pattern for scientific notation
+    pattern_scientific = r'-?\d+(?:\.\d+)?[eE][+-]?\d+'
+    # Pattern for simple numbers without commas
+    pattern_simple = r'-?(?:\d+\.\d+|\.\d+|\d+\b)(?![eE][+-]?\d+)(?![,\d])'
+
+    # Extract numbers with commas
+    numbers_with_commas = re.findall(pattern_commas, string)
+    # Extract numbers in scientific notation
+    numbers_scientific = re.findall(pattern_scientific, string)
+    # Extract simple numbers without commas
+    numbers_simple = re.findall(pattern_simple, string)
+
+    # Combine all extracted numbers
+    all_numbers = numbers_with_commas + numbers_scientific + numbers_simple
+    return all_numbers
+
+def parse_open_response(response):
+    """
+    Parse the prediction from the generated response.
+    Return a list of predicted strings or numbers.
+    """
+    # content = content.strip("\n").strip(".").strip(" ")
+    def get_key_subresponses(response):
+        key_responses = []
+        response = response.strip().strip(".").lower()
+        sub_responses = re.split(r'\.\s(?=[A-Z])|\n', response)
+        indicators_of_keys = ['could be ', 'so ', 'is ',
+                            'thus ', 'therefore ', 'final ', 'answer ', 'result ']
+        key_responses = []
+        for index, resp in enumerate(sub_responses):
+            # if last one, accept it's an equation (the entire response can be just one sentence with equation)
+            if index == len(sub_responses) - 1:
+                indicators_of_keys.extend(['='])
+            shortest_key_response = None # the shortest response that may contain the answer (tail part of the response)
+            for indicator in indicators_of_keys:
+                if indicator in resp:
+                    if not shortest_key_response:
+                        shortest_key_response = resp.split(indicator)[-1].strip()
+                    else:
+                        if len(resp.split(indicator)[-1].strip()) < len(shortest_key_response):
+                            shortest_key_response = resp.split(indicator)[-1].strip()
+                    # key_responses.append(resp.split(indicator)[1].strip())
+            
+            if shortest_key_response:
+                # and it's not trivial
+                if shortest_key_response.strip() not in [":", ",", ".", "!", "?", ";", ":", "'"]:
+                    key_responses.append(shortest_key_response)
+        if len(key_responses) == 0: # did not found any
+            return [response]
+        return key_responses
+    # pdb.set_trace()
+    key_responses = get_key_subresponses(response)
+
+    pred_list = key_responses.copy() # keep the original string response
+    for resp in key_responses:
+        pred_list.extend(extract_numbers(resp))
+
+    tmp_pred_list = []
+    for i in range(len(pred_list)):
+        tmp_pred_list.extend(normalize_str(pred_list[i]))
+    pred_list = tmp_pred_list
+
+    # remove duplicates
+    pred_list = list(set(pred_list))
+
+    return pred_list
+
+# ----------- Evaluation -------------
+
+def eval_multi_choice(gold_i, pred_i):
+    """
+    Evaluate a multiple choice instance.
+    """
+    correct = False
+    # only they are exactly the same, we consider it as correct
+    if isinstance(gold_i, list):
+        for answer in gold_i:
+            if answer == pred_i:
+                correct = True
+                break
+    else: # gold_i is a string
+        if gold_i == pred_i:
+            correct = True
+    return correct
+
+def eval_open(gold_i, pred_i):
+    """
+    Evaluate an open question instance
+    """
+    correct = False
+    if isinstance(gold_i, list):
+        # use float to avoid trivial matches
+        norm_answers = []
+        for answer in gold_i:
+            norm_answers.extend(normalize_str(answer))
+    else:
+        norm_answers = normalize_str(gold_i)
+    for pred in pred_i: # pred is already normalized in parse response phase
+        if isinstance(pred, str): # if it's a string, then find if ans in the pred_i
+            for norm_ans in norm_answers:
+                # only see if the string answer in the string pred
+                if isinstance(norm_ans, str) and norm_ans in pred:
+                    if not correct:
+                        correct = True
+                    break
+        else: # it's a float number
+            if pred in norm_answers:
+                if not correct:
+                    correct = True
+                break
+    return correct
+
+# ----------- Batch Evaluation -------------
+def evaluate(samples):
+    """
+    Batch evaluation for multiple choice and open questions.
+    """
+    pred_correct = 0
+    judge_dict = dict()
+    for sample in samples:
+        gold_i = sample['answer']
+        pred_i = sample['parsed_pred']
+        if sample['question_type'] == 'multiple-choice':
+            correct = eval_multi_choice(gold_i, pred_i)
+        else: # open question
+            correct = eval_open(gold_i, pred_i)
+
+        if correct:
+            judge_dict[sample['id']] = 'Correct'
+            pred_correct += 1
+        else:
+            judge_dict[sample['id']] = 'Wrong'
+
+    if len(samples) == 0:
+        return {'acc': 0}
+    return judge_dict, {'acc': pred_correct / len(samples)}
+
+
+
+# ----------- Calculate Accuracy -------------
+def calculate_ins_level_acc(results: Dict):
+    """Calculate the instruction level accuracy for given Subject results"""
+    acc = 0
+    ins_num = 0
+    for cat_results in results.values():
+        acc += cat_results['acc'] * cat_results['num_example']
+        ins_num += cat_results['num_example']
+    if ins_num == 0:
+        return 0
+    return acc / ins_num
+
+def parse_options(question):
+    """
+    return index to answer option
+    """
+    # Extract the answer options part using regular expression
+    options_text = re.search(r"# Answer option: \[(.*?)\]", question).group(1)
+
+    # Find each option and answer pair
+    options = re.findall(r"([A-E])\)([^,]+)", options_text)
+
+    # Convert the list of tuples into a dictionary
+    answer_dict = {option: answer for option, answer in options}
+
+    return answer_dict
+
+def compute_acc(questions, answers, gts, dataset):
+
+    # Append each question and its highlighted answer to the HTML content
+    total_acc = 0
+    failed_follow_format_question = 0
+    failed_follow_format_final_answer = 0
+
+    for i, (question, answer, gt) in enumerate(zip(questions, answers, gts)):
+        if dataset in ['GSM8K', 'p_GSM8K', 'MultiArith', 'SVAMP', 'ASDiv', 'GSM8K_Hard', 'GSM_Plus']:
+            pred_list = parse_open_response(answer)
+            if gt in pred_list:
+                total_acc += 1
+            else:
+                print("Question: ", question, "Answer: ", answer[-100:], 'GT: ', gt)
+                print("====================================")
+        elif dataset in ['AQUA']:
+            index2ans = parse_options(question)
+            all_choices = list(index2ans.keys())
+            pred_index = parse_multi_choice_response(answer, all_choices, index2ans)
+            
+            if pred_index == gt:
+                total_acc += 1
+        
+        elif dataset in ['date', 'CLUTRR', 'wikimultihopQA']:
+            conclusion = answer.split('{')[-1].split('}')[0]
+            
+            if conclusion.lower() == gt.lower():
+                total_acc += 1
+            else:
+                if  gt.lower() in answer.lower():
+                    total_acc += 1
+                else:
+                    print("Question: ", question, "Answer: ", answer[-100:], 'GT: ', gt)
+                    print("====================================")
+        
+        elif dataset in ['gpqa']:
+            conclusion = answer.split('{')[-1].split('}')[0]
+            
+            try:
+                if float(conclusion) == float(gt):
+                    total_acc += 1
+                else:
+                    print("Question: ", question, "Answer: ", answer[-100:], 'GT: ', gt)
+                    print("====================================")
+            except:
+                continue
+                
+    print("The number of failed to follow the format (question, final answer): ", failed_follow_format_question, failed_follow_format_final_answer)
+    # print(total_iou/len(questions))
+    print(total_acc, len(questions)-failed_follow_format_question-failed_follow_format_final_answer)
+    # print("Accuracy: ", total_acc/len(questions))
+    print("Accuracy: ", total_acc/(len(questions) - failed_follow_format_question - failed_follow_format_final_answer))
+
+if __name__ == '__main__':
+    arg_parser = get_common_args()
+    args = arg_parser.parse_args()
+    
+    # load config file
+    # data_path
+    with open('../configs/config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    base_data_path = config['base_data_path']
+    data_path = os.path.join(base_data_path, config['data_paths'][args.dataset])
+
+    result_folder = '../results_auto_tagging'
+    
+    answer_mode = args.answer_mode
+    if args.answer_mode == 'grounding_cot':
+        answer_mode = 'design_1_v4'
+        
+    if args.data_mode == 'random':
+        df_path = f'{result_folder}/{args.dataset}/{answer_mode}/fs_inst_{args.llm_model}_temp_10_random.csv'
+    else:
+        df_path = f'{result_folder}/{args.dataset}/{answer_mode}/fs_inst_{args.llm_model}_temp_10_longest.csv'
+
+    # df_path = f'../results/{args.dataset}/cot/fs_inst_claude_temp_0.csv'
+
+    df = pd.read_csv(df_path)
+    questions = df['question'].tolist()
+    answers = df['answer'].tolist()
+    ids = df['id'].tolist()
+    
+    if answer_mode == 'design_1_v4':
+        # remove all the tags in the answer
+        answers = [re.sub(r'</?fact\d+>', '', text) for text in answers]
+
+    gts = retrieve_gts(data_path, ids, args.dataset)
+    
+    compute_acc(questions, answers, gts, args.dataset)
