2a8e631c5cb254a7a3065e3df150702b519431f5
tin <ngthanhtinqn@gmail.com>
2024-11-07 08:48:24 -0600

    fix q

--- one_step_grounding.py
+++ one_step_grounding.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 from arg_parser import get_common_args
 from utils.utils import read_jsonl_file, extract_last_sentence
-from agents.api_agents import api_agent, batch_api_agent
+from agents.api_agents import api_agent
+from agents.batch_api_agents import batch_api_agent
 
 random.seed(0)
 
@@ -27,14 +28,14 @@ def create_prompt(question, dataset, prompt_used, few_shot_prompt, answer_mode):
         last_sentence = extract_last_sentence(question)
     
     if prompt_used == "fs":
-        prompt = f"{few_shot_prompt}\n{q}"
+        prompt = f"{few_shot_prompt}\n{question}"
         
     if prompt_used == "fs_inst":
         if answer_mode == 'da':
-            prompt = f"{few_shot_prompt}\n{q}\nDo not generate your explaination, please give the answer only as follow:\n\
+            prompt = f"{few_shot_prompt}\n{question}\nDo not generate your explaination, please give the answer only as follow:\n\
                 Answer:."
         if answer_mode == 'cot':
-            prompt = f"{few_shot_prompt}\n{q}\nPlease generate your explanation first, then generate the final answer in the bracket as follow:\n" +"Answer: {}"
+            prompt = f"{few_shot_prompt}\n{question}\nPlease generate your explanation first, then generate the final answer in the bracket as follow:\n" +"Answer: {}"
         if answer_mode == 'grounding_cot':
             #(ground in Q and A)
             instruction = f"I want you to answer this question but your explanation should contain references referring back to the information in the question. To do that, first, re-generate the question with proper tags for key phrases, the key phrases that are most relevant to answering the question {last_sentence} and then generate your answers. The output format is as follow:\n\
@@ -51,7 +52,7 @@ def create_prompt(question, dataset, prompt_used, few_shot_prompt, answer_mode):
             #     Reformatted Question: \
             #         Answer:"
             
-            prompt = f"{few_shot_prompt}\n{q}\n{instruction}"
+            prompt = f"{few_shot_prompt}\n{question}\n{instruction}"
             
     if prompt_used == "zs":
         instruction = "I want you to answer this question but your explanation should contain references referring back to the information in the question. To do that, first, re-generate the question with proper tags (<fact1>, <fact2>, <fact3>, etc) for key phrases, the key phrases that are most relevant to answering the question {last_sentence}, and then generate your answers that also have the tag (<fact1>, <fact2>, <fact3>, etc) for the grounded information. Give your answer by analyzing step by step, and give your answer in curly brackets" + " {} in the final answer. The output format is as follow:\n\
@@ -59,7 +60,7 @@ def create_prompt(question, dataset, prompt_used, few_shot_prompt, answer_mode):
                 Answer:\
                     Final answer:"
                     
-        prompt = f"{q}\n{instruction}"
+        prompt = f"{question}\n{instruction}"
     
     return prompt
     
