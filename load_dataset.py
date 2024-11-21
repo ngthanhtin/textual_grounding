b3cf82826b1c010378fb0567940d0d12c3d1d3e6
tin <ngthanhtinqn@gmail.com>
2024-11-20 18:54:19 -0600

    1st version of load data

---
+++ load_dataset.py
@@ -0,0 +1,276 @@
+
+import yaml, os, re, random, json
+random.seed(0)
+
+class DatasetLoader:
+    def __init__(self, config_path, base_data_path, base_few_shot_prompt_path, dataset, data_mode='longest', num_samples=200):
+        """
+        Initialize the dataset loader with configuration.
+
+        :param config_path: Path to the YAML config file.
+        :param base_data_path: Base directory for dataset files.
+        :param dataset: Name of the dataset to load.
+        :param data_mode: Mode for data loading ('random' or 'longest').
+        """
+        self.config_path = config_path
+        self.base_data_path = base_data_path
+        self.base_few_shot_prompt_path = base_few_shot_prompt_path
+        self.dataset = dataset
+        self.data_mode = data_mode
+        self.num_samples = num_samples
+        
+        self.config = self._load_config()
+        self.data_path = self._get_data_path()
+        self.data = self._read_data()
+        
+        self.question_length = self.config['max_question_lengths'][self.dataset]
+        self.question_length = 0 # set this to load the whole dataset
+        
+    
+    def _retrieve_gts(self, ids):
+        """
+        ids: ids of the predictions (list)
+        """        
+        # read gt
+        gts = []
+        if self.dataset in ['coin', 'last_letter_2', 'last_letter_4', 'GSM8K_Hard']:
+            for id in ids:
+                for i, temp in enumerate(data):
+                    if id == i:
+                        # print(temp['new_question'], id, temp['answer'])
+                        # exit()
+                        gt = temp['answer']
+                        if self.dataset == 'coin':
+                            if gt == 'yes':
+                                gt = True
+                            else:
+                                gt = False
+                        gts.append(gt)
+            return gts
+        
+        if self.dataset in ['GSM_Plus', 'GSM_IC']:
+            if self.dataset == 'GSM_IC':
+                length = 583 # 370: 2 step, 583: mstep
+                questions = [x['new_question'] for x in data if len(x["new_question"]) >= length]
+                answers = [x['answer'] for x in data if len(x["new_question"]) >= length]
+                data = [{'new_question': q, 'answer': a} for q, a in zip(questions, answers)]
+            num_cannot_convert = 0
+            num_none = 0
+            for id in ids:
+                for i, temp in enumerate(data):
+                    if id == i:
+                        # print(temp['new_question'], id, temp['answer'])
+                        # exit()
+                        gt = temp['answer']
+                        if temp['answer'] == 'None':
+                            gts.append(None)
+                            num_none += 1
+                        else:
+                            try:
+                                gts.append(float(gt))
+                            except:
+                                num_cannot_convert += 1
+                                gts.append(None)
+                                num_none += 1
+            print(num_none)
+            print(num_cannot_convert)
+            # exit()
+            
+            return gts
+    
+        for id in ids:
+            for temp in data:
+                if self.dataset == 'p_GSM8K':
+                    if temp['index'] == id:
+                        gt = temp['answer']
+                        gts.append(float(gt))
+                elif self.dataset == 'wikimultihopQA':
+                    if temp['_id'] == id:
+                        gt = temp['answer']
+                        gts.append(gt)
+                elif self.dataset == 'spartQA':
+                    if temp['id'] == id:
+                        gt = temp['answer']
+                        if gt == 0:
+                            gt = 'A'
+                        elif gt == 1:
+                            gt = 'B'
+                        elif gt == 2:
+                            gt = 'C'
+                        elif gt == 3:
+                            gt = 'D'
+                        gts.append(gt)
+                elif self.dataset == 'reclor':
+                    if temp['id_string'] == id:
+                        gt = temp['label']
+                        if gt == 0:
+                            gt = 'A'
+                        elif gt == 1:
+                            gt = 'B'
+                        elif gt == 2:
+                            gt = 'C'
+                        elif gt == 3:
+                            gt = 'D'
+                        gts.append(gt)
+                elif self.dataset == 'commonsenseQA':
+                    if temp['id'] == id:
+                        gt = temp['answerKey']
+                        gts.append(gt)
+                else:
+                    if temp['id'] == id:
+                        gt = temp['answer']
+                        if self.dataset in ['GSM8K', 'p_GSM8K', 'MultiArith', 'SVAMP']:
+                            gt = gt.split('####')[1].strip()
+                            if ',' in gt:
+                                gt = gt.replace(',', '')
+                            gts.append(float(gt))
+                        if self.dataset == 'CLUTRR':
+                            gt = gt.split('####')[1].strip()
+                            gts.append(gt)
+                        if self.dataset == 'date':
+                            gt = gt.split('####')[1].strip()
+                            gts.append(gt)
+                        if self.dataset == 'ASDiv':
+                            #if gt is list, convert it to string
+                            if type(gt) == list:
+                                gt = gt[0]
+                            gt = gt.replace(',', '')
+                            gts.append(float(gt))
+                        if self.dataset in ['StrategyQA', 'navigate', 'causal_judgement', 'web_of_lies']:
+                            if gt == 'yes':
+                                gt = True
+                            else:
+                                gt = False
+                            gts.append(gt)
+                        if self.dataset in ['AQUA', 'logical_deduction_three_objects', 'logical_deduction_five_objects', 'logical_deduction_seven_objects', 'reasoning_about_colored_objects', 'word_sorting', 'tracking_shuffled_objects_three_objects', 'tracking_shuffled_objects_five_objects', 'tracking_shuffled_objects_seven_objects', 'temporal_sequences']:
+                            gts.append(gt)    
+                        if self.dataset == 'object_counting':
+                            gts.append(float(gt))
+        
+        return gts
+    
+    def _load_few_shot_prompt(self):
+        fewshot_prompt_path = os.path.join(self.base_few_shot_prompt_path, self.config['prompts']['design_1_v4'][self.dataset])
+        with open(fewshot_prompt_path, 'r') as file:
+            few_shot_prompt = file.read()
+        return few_shot_prompt
+    
+    def _load_config(self):
+        with open(self.config_path, 'r') as f:
+            return yaml.safe_load(f)
+
+    def _get_data_path(self):
+        return os.path.join(self.base_data_path, self.config['data_paths'][self.dataset])
+
+    def _read_data(self):
+        if 'jsonl' in self.data_path:
+            return self._read_jsonl_file(self.data_path)
+        else:
+            with open(self.data_path, 'r') as file:
+                return json.load(file)
+
+    @staticmethod
+    def _read_jsonl_file(path):
+        with open(path, 'r') as file:
+            return [json.loads(line) for line in file]
+
+    def get_questions_and_ids(self):
+        """
+        Process and extract questions and IDs based on dataset type.
+        """
+        
+        if self.dataset == 'p_GSM8K':
+            return self._process_generic('new_question', 'index')
+        elif self.dataset == 'commonsenseQA':
+            return self._process_commonsenseQA()
+        elif self.dataset == 'spartQA':
+            return self._process_spartQA()
+        elif self.dataset == 'reclor':
+            return self._process_reclor()
+        elif self.dataset in ['GSM_IC', 'GSM8K_Hard', 'GSM_Plus', 'coin', 'last_letter_2', 'last_letter_4']:
+            return self._process_simple()
+        elif self.dataset == 'wikimultihopQA':
+            return self._process_generic('question', '_id')
+        else:
+            return self._process_generic('question', 'id')
+
+    def _process_generic(self, question_key, id_key):
+        questions = [x[question_key] for x in self.data if len(x[question_key]) >= self.question_length]
+        ids = [x[id_key] for x in self.data if len(x[question_key]) >= self.question_length]
+        return questions, ids
+
+    def _process_commonsenseQA(self):
+        full_questions, full_ids = []
+        max_len_questions, max_len_ids = [], []
+        random_questions, random_ids = [], []
+        
+        for sample in self.data:
+            question = sample['question']['stem']
+            choices = sample['question']['choices']
+            answer_choices = ''.join(
+                f"({choice['label'].lower()}) {choice['text']}\n" for choice in choices
+            )
+               
+            full_questions.append(question + "\n" + answer_choices)
+            full_ids.append(sample['id'])
+            
+            if len(question) >= self.question_length:
+                max_len_questions.append(question + "\n" + answer_choices)
+                max_len_ids.append(sample['id'])
+        
+        # get randoms except max_len_questions
+        for i, question in enumerate(full_questions):
+            if question not in max_len_questions:
+                random_questions.append(question)
+                random_ids.append(full_ids[i])
+        
+        indices = random.sample(range(len(random_questions)), self.num_samples)
+
+        # Create subsets based on the selected indices
+        random_questions = [random_questions[i] for i in indices]
+        random_ids = [random_ids[i] for i in indices]
+        
+        if self.data_mode == 'longest':
+            return max_len_questions, max_len_ids
+        else:
+            return random_questions, random_ids
+
+    def _process_spartQA(self):
+        questions = [
+            x['question'].replace('0:', '(a)').replace('1:', '(b)').replace('2:', '(c)').replace('3:', '(d)')
+            for x in self.data if len(x["question"]) >= self.question_length
+        ]
+        ids = [x["id"] for x in self.data if len(x["question"]) >= self.question_length]
+        
+        # take 400 random questions and ids
+        combined = list(zip(questions, ids))
+        sampled = random.sample(combined, k=400)
+
+        questions, ids = zip(*sampled)
+
+        questions = list(questions)
+        ids = list(ids)
+        
+        return questions, ids
+
+    def _process_reclor(self):
+        questions = [
+            x['context'] + ' ' + x["question"] +
+            '\n(a) ' + x['answers'][0] +
+            '\n(b) ' + x['answers'][1] +
+            '\n(c) ' + x['answers'][2] +
+            '\n(d) ' + x['answers'][3]
+            for x in self.data if len(x["question"]) >= self.question_length
+        ]
+        ids = [x["id_string"] for x in self.data if len(x["question"]) >= self.question_length]
+        return questions, ids
+
+    def _process_simple(self):
+        questions = [
+            x['new_question'] if 'new_question' in x else x['question']
+            for x in self.data if len(x.get('new_question', x.get('question', ''))) >= self.question_length
+        ]
+        ids = [i for i in range(len(questions))]
+        return questions, ids  
+        
+    
